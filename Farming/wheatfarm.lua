76f65a93058677cf8bac8ab03dc478b45b635633
Noah Kamara <40308075+noahkamara@users.noreply.github.com>
2020-11-04 21:20:24 +0100

    Update wheatfarm.lua

--- Farming/wheatfarm.lua
+++ Farming/wheatfarm.lua
@@ -84,7 +84,9 @@ end
 local function getFieldSize()
     if fs.exists("farm.cfg") then
         local file = fs.open("farm.cfg", "r")
-        local cfg = textutils.unserialise(file.readAll())
+        local content = file.readAll()
+        file.close()
+        local cfg = textutils.unserialise(content)
         if cfg then
             return tonumber(cfg.size)
         end
