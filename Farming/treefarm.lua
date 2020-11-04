0b7007ce9a0fe34a9cceafed2fadc6c93b00e3ee
tilman <tilman.grueger@web.de>
2020-11-04 15:38:57 +0100

    t

--- Farming/treefarm.lua
+++ Farming/treefarm.lua
@@ -1,3 +1,16 @@
+
+function refuel()
+    for i=1, 16 do
+        local data = turtle.getItemDetail(i)
+        if data and string.match(data['name'], "coal") then
+            turtle.select(i)
+            turtle.refuel(2)
+            return true
+        end
+    end
+    return false
+end
+
 local function walk(length)
     if length == nil then
         length = 1
@@ -15,18 +28,6 @@ local function walk(length)
     end
 end
 
-function refuel()
-    for i=1, 16 do
-        local data = turtle.getItemDetail(i)
-        if data and string.match(data['name'], "coal") then
-            turtle.select(i)
-            turtle.refuel(2)
-            return true
-        end
-    end
-    return false
-end
-
 function find_item(item_name)
     -- findet ersten Slot des strings "item_name" und waehlt ihn aus
 
@@ -41,23 +42,17 @@ function find_item(item_name)
 end
 
 
-local function walk_tree()
-
-    local success, data = turtle.inspect()
-    in_tree = true
-    fuellevel = turtle.getFuelLevel()
-                if fuellevel < 10 then
-                    refuel()
-                 end
-    if success then 
+local function cut_leaves()
+    for i = 1 , 4 do
         turtle.dig()
         turtle.forward()
-        if  string.match(data.name, "log") == "log" then
-            chop_tree()
-            turtle.digDown()
-            find_item("sapling")
-            turtle.placeDown()
+        for i = 1, 4 do
+            turtle.dig()
+            turtle.turnRight()
         end
+        turtle.dig()
+        turtle.back()
+        turtle.turnRight()
     end
 end
 
@@ -80,26 +75,33 @@ local function chop_tree()
     end
 end
 
-local function cut_leaves()
-    for i = 1 , 4 do
+local function walk_tree()
+
+    local success, data = turtle.inspect()
+    in_tree = true
+    fuellevel = turtle.getFuelLevel()
+                if fuellevel < 10 then
+                    refuel()
+                 end
+    if success then 
         turtle.dig()
         turtle.forward()
-        for i = 1, 4 do
-            turtle.dig()
-            turtle.turnRight()
+        if  string.match(data.name, "log") == "log" then
+            chop_tree()
+            turtle.digDown()
+            find_item("sapling")
+            turtle.placeDown()
         end
-        turtle.dig()
-        turtle.back()
-        turtle.turnRight()
     end
 end
+
 
 anzahl_baume_quer = 4
 anzahl_baume_tiefe = 4
 abstand_zw_baumen = 5 --inclusive eigner stamm
 
 if anzahl_baume_tiefe % 2 == 0 then
-    walk_tree_back == true
+    walk_tree_back = true
 end
 
 walk_tree()
