2abc13023aa82b40b66ef568ae8bb1d04d634402
tilman <tilman.grueger@web.de>
2020-11-04 15:29:56 +0100

    for

--- Farming/treefarm.lua
+++ Farming/treefarm.lua
@@ -2,7 +2,7 @@ local function walk(length)
     if length == nil then
         length = 1
     end
-    for i, length do
+    for i = 1, length do
         local success = false
         while not success do
             fuellevel = turtle.getFuelLevel()
@@ -75,16 +75,16 @@ local function chop_tree()
         y, data = turtle.inspectUp()
         h = h + 1
     until  string.match(data.name, wood) ~= wood
-    for i, h do
+    for i =1, h do
         turtle.down()
     end
 end
 
 local function cut_leaves()
-    for i, 4 do
+    for i = 1 , 4 do
         turtle.dig()
         turtle.forward()
-        for i, 4 do
+        for i = 1, 4 do
             turtle.dig()
             turtle.turnRight()
         end
@@ -104,19 +104,19 @@ end
 
 walk_tree()
 
-for q, anzahl_baume_quer-1 do
-    for t, anzahl_baume_tiefe-1*abstand_zw_baumen do
+for q = 1, anzahl_baume_quer-1 do
+    for t = 1 , anzahl_baume_tiefe-1*abstand_zw_baumen do
         walk_tree()
     end
     if q %2 ~= 0  then
         turtle.turnLeft()
-        for a, abstand_zw_baumen do
+        for a=1, abstand_zw_baumen do
             walk_tree()
         end
         turtle.turnLeft()
     else
         turtle.turnRight()
-        for a, abstand_zw_baumen do
+        for a =1 , abstand_zw_baumen do
             walk_tree()
         end
         turtle.turnRight()
