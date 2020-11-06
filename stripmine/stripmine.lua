1b08419a24495ae6c635fd2c28f061b0146f657a
tilman <tilman.grueger@web.de>
2020-11-06 16:07:20 +0100

    t

--- stripmine/stripmine.lua
+++ stripmine/stripmine.lua
@@ -61,7 +61,6 @@ local function place_chest_and_fill()
         if data ~= nil then
             if string.match(data["name"], "chest") ~= "chest" and string.match(data["name"], "coal") ~= "coal" and string.match(data["name"], "torch") ~= "torch" then
                 turtle.drop()
-            end
             elseif string.match(data["name"], "coal") == "coal" then
                 coal_stacks = coal_stacks + 1
             end
