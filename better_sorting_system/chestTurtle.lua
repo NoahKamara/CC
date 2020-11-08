244222c55cb2ad15570ffc21e7c726fb63265cde
Sören Michaels <soeren.michaels@gmail.com>
2020-11-08 21:23:34 +0100

    update

--- better_sorting_system/chestTurtle.lua
+++ better_sorting_system/chestTurtle.lua
@@ -3,13 +3,21 @@ function getMaxItemsInChest()
 end
 
 function fillChest(count)
-    while count ~= 0 do
+    break_condition = true
+    while count ~= 0 and break_condition do
         for i = 2, 16 do
             turtle.select(i)
             countItemSlot = turtle.getItemCount()
-            if countItemSlot > count then break end
+            if countItemSlot > count then
+                break_condition = false
+                break
+            end
             count = count - countItemSlot
             turtle.dropDown()
+        end
+        if break_condition then
+            print("Error chest has less items than count")
+            return 0
         end
     end
     turtle.dropDown(count)
