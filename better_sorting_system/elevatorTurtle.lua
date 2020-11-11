be52b333c6428a96d1bf908a015a465f3de7a4da
tilman <tilman.grueger@web.de>
2020-11-11 21:24:27 +0100

    t

---
+++ better_sorting_system/elevatorTurtle.lua
@@ -0,0 +1,34 @@
+
+function empty_input_chest()
+    for i = 2, 16 do
+        turtle.select(i)
+        turtle.suck()
+    end
+end
+
+function drop_in_input_chest()
+    for i = 2, 16 do
+        turtle.select(i)
+        turtle.suck()
+    end
+end
+
+function to_floor(cur_floor, dest_floor)
+    for i=1, math.abs(cur_floor-dest_floor) * hight_of_floor do
+        if cur_floor-dest_floor == math.abs(cur_floor-dest_floor) then
+            turtle.up()
+        elseif cur_floor-dest_floor ~= math.abs(cur_floor-dest_floor) then
+            turtle.down()
+        end
+    end
+end
+
+function drop_in_floorturtle(inventory_slots)
+    for i in invertory_slots do
+        turtle.select(i)
+        turtle.drop()
+    end
+end
+
+cur_floor = 0
+hight_of_floor
