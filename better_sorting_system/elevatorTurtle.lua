f95b02c57acdb40e3ecbc78a41762ca0aa99f03d
tilman <tilman.grueger@web.de>
2020-11-11 22:29:27 +0100

    t

--- better_sorting_system/elevatorTurtle.lua
+++ better_sorting_system/elevatorTurtle.lua
@@ -1,11 +1,14 @@
 
 function empty_input_chest()
+    while turtle.suck() do end
+    inventory = {}
     for i = 2, 16 do
+        inventory[i] = {}
         turtle.select(i)
-        turtle.suck()
-        turtle.getItemCount()
-        turtle.getItemDetail()
-    end
+        inventory.i ["count"] = turtle.getItemCount()
+        inventory.i ["item"] = turtle.getItemDetail().name    
+    end 
+    return inventory
 end
 
 function drop_in_input_chest()
@@ -17,20 +20,29 @@ end
 
 function to_floor(cur_floor, dest_floor)
     for i=1, math.abs(cur_floor-dest_floor) * hight_of_floor do
-        if cur_floor-dest_floor == math.abs(cur_floor-dest_floor) then
+        if cur_floor - dest_floor == math.abs(cur_floor - dest_floor) then
             turtle.up()
-        elseif cur_floor-dest_floor ~= math.abs(cur_floor-dest_floor) then
+        elseif cur_floor - dest_floor ~= math.abs(cur_floor - dest_floor) then
             turtle.down()
         end
     end
 end
 
 function drop_in_floorturtle(inventory_slots)
-    for i in invertory_slots do
+    for i = 2, 16 do
         turtle.select(i)
-        turtle.drop()
+        if i in inventory_slots then
+            turtle.drop()
+        end
     end
 end
+
+function deliver(inventory)
+    for 
+        to_floor(cur_floor, inventory["z"])
+
+
+
 
 cur_floor = 0
 hight_of_floor = 1
