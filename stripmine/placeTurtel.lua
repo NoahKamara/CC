9d4984c235e7d0805f86b7b699ef5ec5ccbe7105
T-136 <tilman.grueger@web.de>
2021-12-24 13:25:21 +0100

    t

--- stripmine/placeTurtel.lua
+++ stripmine/placeTurtel.lua
@@ -1,31 +1,31 @@
 
 print("Kobble in slot 1")
-print("KOhle in slot 5")
+print("KOhle in slot 4")
 
-print("Anzahl der Turtle in slot 2")
+print("Anzahl der Turtle in slot 5 ff")
 local turtleAmmount = tonumber(read())
 
-print("Anzahl der Kisten in slot 3")
+print("Anzahl der Kisten in slot 2")
 local chestAmmount = tonumber(read())
 
-print("Anzahl der Fackeln in slot 4")
+print("Anzahl der Fackeln in slot 3")
 local torcheAmmount = tonumber(read())
 
 
-for i = 1, turtleAmmount do     
-    turtle.select(1)
+for turtleNumber = 1, turtleAmmount do     
+    turtle.select(turtleNumber+4)
     while (turtle.place() == false) do
         os.sleep(3)
     end
 
-    for i=2, 5 do
-        if i == 2 then
+    for i=1, 4 do
+        if i == 1 then
             drop = 20
+        elseif i == 2 then 
+            drop = math.floor( chestAmmount / turtleAmmount)
         elseif i == 3 then 
-            drop = math.floor(torcheAmmount / turtleAmmount)
+            drop = math.floor( torcheAmmount / turtleAmmount)
         elseif i == 4 then 
-            drop = math.floor(chestAmmount / turtleAmmount)
-        elseif i == 5 then 
             drop = 1
         end
 
