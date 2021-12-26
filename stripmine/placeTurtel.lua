9a065ffbefb1318084682c7711159b7a5f781b4d
T-136 <tilman.grueger@web.de>
2021-12-26 01:04:40 +0100

    more fuel

--- stripmine/placeTurtel.lua
+++ stripmine/placeTurtel.lua
@@ -4,7 +4,7 @@ os.loadAPI("basics.lua")
 print("Kobble in slot 1")
 print("Treibstoff in slot 4")
 
-print("Anzahl der Turtle in slot 5 ff")
+print("Anzahl der Turtle in slot 7 ff")
 local turtleAmmount = tonumber(read())
 
 print("Anzahl der Kisten in slot 2")
@@ -13,11 +13,14 @@ local chestAmmount = tonumber(read())
 print("Anzahl der Fackeln in slot 3")
 local torcheAmmount = tonumber(read())
 
-print("Starttreibstoff pro Turtle in slot 4")
+print("Starttreibstoff pro Turtle in slot 4-6")
 local treibstoff = tonumber(read())
 
+local stack = 64
+local stackNumber = 0
+
 for turtleNumber = 1, turtleAmmount do     
-    turtle.select(turtleNumber+4)
+    turtle.select(turtleNumber+6)
     while (turtle.place() == false) do
         os.sleep(3)
     end
@@ -26,18 +29,31 @@ for turtleNumber = 1, turtleAmmount do
         if i == 1 then
             drop = 2
         elseif i == 2 then 
-            drop = math.floor( chestAmmount / turtleAmmount)
+            drop = math.floor( chestAmmount / (turtleAmmount + 1))
         elseif i == 3 then 
-            drop = math.floor( torcheAmmount / turtleAmmount)
-        elseif i == 4 then 
-            drop = treibstoff
+            drop = math.floor( torcheAmmount / (turtleAmmount + 1))
         end
-
+        
         turtle.select(i)
         turtle.drop(drop)
+
+        if i==4 then 
+            stack = stack - treibstoff
+            if stack > 0 then 
+                turtle.select(i+stackNumber)
+                turtle.drop(treibstoff)
+            elseif stack < 0 then
+                turtle.select(i+stackNumber)
+                turtle.drop()
+                stackNumber = stackNumber + 1
+                turtle.select(i+stackNumber)
+                turtle.drop(math.abs(stack))
+                stack = 64 - math.abs(stack)
+            end
+        end
     end
 end
 
-if basics.walk() == "turtle" then os.sleep(3) end 
+while (basics.walk() == "turtle") do os.sleep(3) end 
 
 shell.run("stripmine/startup.lua")
